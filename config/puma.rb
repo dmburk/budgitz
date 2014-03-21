457a9887f40cc11e70a79582165f7ab6f5a62559
Michael Burk <michael.burk@gmail.com>
2014-03-21 12:51:36 -0400

    Removed puma. Added unicorn as app server. Restructured gemfile.

--- config/puma.rb
+++
@@ -1,9 +0,0 @@
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-threads 4,4
-
-bind "unix:///data/apps/appname/shared/tmp/puma/appname-puma.sock"
-pidfile "/data/apps/appname/current/tmp/puma/pid"
-state_path "/data/apps/appname/current/tmp/puma/state"
-
-activate_control_app

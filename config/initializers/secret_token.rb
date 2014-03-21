9d1df7cd2198ff9e22c905d97d28dbb2eb41aa67
Michael Burk <michael.burk@gmail.com>
2014-03-21 12:21:41 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgitz::Application.config.secret_key_base = '5fe8976f62bd8abfc147226bf3b01d5c3035c521fb8d1a8d9caddd7a707cc328f674fdad4910ce1dcf1ea380ee2062a79575e9942ccc868b21d5050fb54bd8cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Budgitz::Application.config.session_store :cookie_store, key: '_budgitz_session'
